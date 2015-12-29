33a3a8fc87494ecd823e3ea9953811d182ff6d49
Khen Price <khen@humtap.com>
2015-12-29 11:08:09 +0200

    truncate to 32 words, not 32 chars, updated css

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,6 +1,9 @@
 module PostsHelper
-  # handles description content
+  @@blurb_max_num_words = 32
+
   def pretify(str)
-    truncate(str, :length => 30)
+  	word_num = @@blurb_max_num_words - 1
+  	str_words = str.split
+  	str_words[0..word_num-1].join(' ')
   end
 end
